cb897fcd58882756409ca406ca06fec00bdf0964
AJ Coppa <aj.coppa@gmail.com>
2014-03-28 20:22:36 -0400

    Started localMax

---
+++ 2014-03-12-localMax.hs
@@ -0,0 +1,24 @@
+-- | localMax Given an entry list, it outputs the its list of local maxima.
+-- A Local maximum is a an element greater than its predecessor and than its
+-- successor.
+--
+-- No point-free today, at least for my solution.
+--
+-- Examples:
+--
+-- >>> localMax [0 .. 1000]
+-- []
+--
+-- >>> localMax [1000 .. 0]
+-- []
+--
+-- >>> localMax [2,2,1,5,4]
+-- [5]
+--
+-- >>> take 4 . localMax $ [0..] >>= (\y -> [y,y+2])
+-- [2,3,4,5]
+--
+localMax :: Ord a => [a] -> [a]
+localMax = undefined
+
+main = print $ localMax [2,2,1,5,4] -- [5]
