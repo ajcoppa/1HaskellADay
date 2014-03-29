787a9bd95b0dba1a73e44a0bdcf617ca4f153e69
AJ Coppa <aj.coppa@gmail.com>
2014-03-29 09:50:04 -0400

    Added wedding

---
+++ 2014-03-18-wedding.hs
@@ -0,0 +1,39 @@
+
+-- $setup
+-- >>> import Data.Maybe
+-- >>> let backPartner = (>>= partner) . (>>= partner)
+
+data Person a = Single a | Married a (Person a)
+
+partner :: Person a -> Maybe (Person a)
+partner (Married _ p) = Just p
+partner _ = Nothing
+
+get :: Person a -> a
+get (Single x)    = x
+get (Married x _) = x
+
+-- | wedding
+-- Marry single people, linking them together
+-- Nothing if one is married
+--
+-- If you're used to Haskell, this one should be VERY easy.
+-- But remember how strange it was the first time...
+-- And see you tomorrow!
+--
+-- Examples:
+--
+-- >>> isNothing $ wedding (Married "foo" (Single "foobar")) (Single "bar")
+-- True
+--
+-- prop> \(x,y) -> (fmap get . backPartner . fmap fst $ wedding (Single x) (Single y)) == Just (x :: String)
+-- prop> \(x,y) -> (fmap get . backPartner . fmap snd $ wedding (Single x) (Single y)) == Just (y :: String)
+
+wedding :: Person a -> Person a -> Maybe (Person a, Person a)
+wedding (Single a) (Single b) =
+  let marriedA = Married a (marriedB)
+      marriedB = Married b (marriedA)
+  in Just (marriedA, marriedB)
+wedding _ _ = Nothing
+
+main = print ""
