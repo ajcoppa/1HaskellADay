3874ec3c825cb0bdedc0c31e7bdfc2426e69c83e
AJ Coppa <aj.coppa@gmail.com>
2014-03-30 17:39:10 -0400

    Use built-in min and max functions

--- 2014-03-21-minmax.hs
+++ 2014-03-21-minmax.hs
@@ -29,8 +29,8 @@ minmax (x:xs) = minmaxHelper x x xs
 minmaxHelper :: Ord a => a -> a -> [a] -> Maybe (a,a)
 minmaxHelper currentMin currentMax [] = Just (currentMin, currentMax)
 minmaxHelper currentMin currentMax (x:xs) =
-  let newMin = if x < currentMin then x else currentMin
-      newMax = if x > currentMax then x else currentMax
+  let newMin = min x currentMin
+      newMax = max x currentMax
   in minmaxHelper newMin newMax xs
 
 main = print $ minmax [0..10]
