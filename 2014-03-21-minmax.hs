33d736eb6665b119857d5696532387a9532e0571
AJ Coppa <aj.coppa@gmail.com>
2014-03-30 15:47:31 -0400

    Added minmax

---
+++ 2014-03-21-minmax.hs
@@ -0,0 +1,27 @@
+-- $setup
+-- >>> import Test.QuickCheck
+-- >>> import Data.Maybe (fromJust)
+
+-- | minmax
+-- get apair of the min and max element of a list (in one pass)
+-- returns Nothing on empty list
+--
+-- Point-free: checked
+--
+-- The function signature follows the idea of the methods in the System.Random
+-- module: given a standard generator, you returns the modified list and the
+-- generator in an altered state.
+--
+-- >>> minmax [0..10]
+-- Just (0,10)
+--
+-- >>> minmax []
+-- Nothing
+--
+-- prop> \(NonEmpty(xs)) -> minimum xs == (fst . fromJust . minmax) xs
+-- prop> \(NonEmpty(xs)) -> maximum xs == (snd . fromJust . minmax) xs
+--
+minmax :: Ord a => [a] -> Maybe (a,a)
+minmax = undefined
+
+main = print $ minmax [0..10]
