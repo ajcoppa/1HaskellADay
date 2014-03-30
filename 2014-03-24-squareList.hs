60d515e3aeabd1fc828d71a43b56f9df070d9bb4
AJ Coppa <aj.coppa@gmail.com>
2014-03-30 17:41:26 -0400

    Added squareList

---
+++ 2014-03-24-squareList.hs
@@ -0,0 +1,15 @@
+-- | squareList builds a list of x lists of size x from a given list of elements
+-- If there aren't enough elements, fill the square with the second parameter
+-- Examples:
+--
+-- >>> squareList 2  0 [0..]
+-- [[0,1],[2,3]]
+--
+-- >>> squareList 2 0 [1]
+-- [[1,0],[0,0]]
+--
+-- >>> squareList 3 () $ repeat ()
+-- [[(),(),()],[(),(),()],[(),(),()]]
+--
+squareList :: Int -> a -> [a] -> [[a]] 
+squareList = undefined
