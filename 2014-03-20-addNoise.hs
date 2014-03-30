04e6d6654c527064a549c172426c82cda3c481c1
AJ Coppa <aj.coppa@gmail.com>
2014-03-30 15:45:36 -0400

    Finished addNoise

--- 2014-03-20-addNoise.hs
+++ 2014-03-20-addNoise.hs
@@ -12,5 +12,33 @@
 -- module: given a standard generator, you returns the modified list and the
 -- generator in an altered state.
 --
+import System.Random
+
 addNoise :: (Num a, Random a) => a -> [a] -> StdGen -> ([a], StdGen)
-addNoise = undefined
+addNoise _ [] gen = ([], gen)
+addNoise noise (x:xs) gen =
+  let range = calculateRange noise
+      startingPair = scanner range (0, gen) x
+      valueGenPairsWithNoiseAdded = scanl (scanner range) startingPair xs
+      valuesWithNoiseAdded = map fst valueGenPairsWithNoiseAdded
+      lastGen = snd $ last valueGenPairsWithNoiseAdded
+  in (valuesWithNoiseAdded, lastGen)
+
+calculateRange :: (Num a) => a -> (a, a)
+calculateRange x = let positiveX = abs x
+  in (-positiveX, positiveX)
+
+scanner :: (Num a, Random a) => (a, a) -> (a, StdGen) -> a -> (a, StdGen)
+scanner range lastValue x =
+  let gen = snd lastValue
+      noiseAndNewGen = randomR range gen
+      noise = fst noiseAndNewGen
+      newGen = snd noiseAndNewGen
+      newX = noise + x
+  in (newX, newGen)
+
+main = do
+  gen <- getStdGen
+  let xs = iterate (*10) 1
+  print $ take 10 $ fst $ addNoise 5 (xs :: [Int]) gen
+  print $ fst $ addNoise 1 ([] :: [Int]) gen
