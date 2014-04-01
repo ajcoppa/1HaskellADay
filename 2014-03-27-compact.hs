1a2810283dc98dda5f5aaa67c5b5688718592457
AJ Coppa <aj.coppa@gmail.com>
2014-03-31 21:55:10 -0400

    Finished compact

--- 2014-03-27-compact.hs
+++ 2014-03-27-compact.hs
@@ -23,5 +23,22 @@
 -- prop> [succ x, x] == (compact . replicate 3 . Just) (x :: Int)
 -- prop> replicate 2 (succ x) == (compact . replicate 4 . Just) (x :: Int)
 --
+import Data.Maybe (fromJust)
+
 compact :: (Enum a, Eq a) => [Maybe a] -> [a]
-compact = undefined
+compact = map fst . foldr folder [] . map fromJust . filter (/= Nothing)
+
+-- Produces a list of pairs.
+-- The Bool value indicates whether this value was already compacted
+-- so we can avoid compacting a given element twice.
+folder :: (Enum a, Eq a) => a -> [(a,Bool)] -> [(a, Bool)]
+folder x accum =
+  if null accum || snd (head accum) || fst (head accum) /= x
+  then (x, False) : accum
+  else (succ x, True) : tail accum
+
+main = do
+  print $ compact [Just 1, Nothing, Just 1, Just 2, Just 4, Just 3, Just 3]
+  print $ compact ([Nothing, Nothing, Nothing] :: [Maybe Int])
+  print $ compact ([] :: [Maybe Int])
+  print $ compact $ map Just "hello"
