c81ceffbc2ee7d18d1c3ad254e539408d91df319
AJ Coppa <aj.coppa@gmail.com>
2014-04-01 20:28:58 -0400

    Replaced pushTo with correct start (previous was missing necessary methods)

--- 2014-03-28-pushTo.hs
+++ 2014-03-28-pushTo.hs
@@ -1,27 +1,20 @@
-import Data.List (group, groupBy, transpose)
-import Data.Maybe (maybe, listToMaybe, catMaybes)
-import Control.Applicative ((<*>), (<$>))
+import Data.List (transpose)
+import Data.Maybe (catMaybes)
+import Control.Applicative ((<*>))
 import Control.Monad (liftM, replicateM)
 
 import Test.QuickCheck
 
--- Preamble
-
 -- $setup
 -- >>> import Control.Applicative ((<$>), (<*>))
--- >>> import Data.List (group, sort)
+-- >>> import Data.List (sort)
+-- >>> import Data.Maybe (catMaybes)
 -- >>> :{
 --   let checkReverse d1 d2 =
 --     (==) <$>
 --        sort . map sort . getList . viewFrom d1 <*>
 --        sort . map (sort . reverse) . getList . viewFrom d2 
 -- :}
-
-newtype Board a = Board {getList :: [[a]]}
-  deriving (Eq, Show)
-
-data Direction = North | South | East | West
-  deriving (Eq, Read, Show)
 
 -- pushTo Push elements of the board to one direction, and compact them
 -- (with a behavior similar to the one of the compact function defined
@@ -30,14 +23,66 @@ data Direction = North | South | East | West
 -- (yes, it is the same behavior as 2048):
 -- http://gabrielecirulli.github.io/2048/
 --
-pushTo :: Direction -> Board (Maybe Int) -> Board (Maybe Int)
+-- >>> getList . pushTo West $ Square [[Just 1, Just 1], [Just 2, Just 2]]
+-- [[Just 2,Nothing],[Just 3,Nothing]]
+--
+-- >>> getList . pushTo North $ Square [[Just 1, Just 1], [Just 2, Just 2]]
+-- [[Just 1,Just 1],[Just 2,Just 2]]
+--
+-- >>> getList . pushTo West $ Square [[Just 1, Just 1], [Just 2, Just 2]]
+-- [[Just 2,Nothing],[Just 3,Nothing]]
+--
+-- prop> :{ \(d, bs) ->
+--    (==)
+--    <$> map compact . getList . viewFrom d
+--    <*> catMaybes . videFrom d . pushTo d
+--     $  bs
+-- :}
+pushTo :: (Enum a, Eq a) => Direction -> Board (Maybe a) -> Board (Maybe a)
 pushTo = undefined
+
+
+-- Old stuff
+
+newtype Board a = Board {getList :: [[a]]}
+  deriving (Eq, Show)
+
+data Direction = North | South | East | West
+  deriving (Eq, Read, Show)
+
+
+-- | viewFrom given a direction, produce an involution such that the
+-- inner lists elements are ordered as if they were seen from that direction.
+--
+--
+-- Examples:
+--
+-- Defaut view is from West
+-- prop> xs == viewFrom West xs
+--
+-- The function is an involution
+-- prop> \(d,xxs) -> (==) <*> (viewFrom d . viewFrom d) $ (xxs :: Board Int)
+--
+-- Ordering properties from opposite side views (for inner lists elements
+-- prop> checkReverse West  East  (xxs :: Board Int)
+-- prop> checkReverse East  West  (xxs :: Board Int)
+-- prop> checkReverse North South (xxs :: Board Int)
+-- prop> checkReverse South North (xxs :: Board Int)
+--
+viewFrom :: Direction -> Board a -> Board a
+viewFrom d = let
+  go West  = id
+  go East  = reverse . map reverse
+  go North = transpose
+  go South = reverse . map reverse . transpose
+  in Board . go d . getList
+
 
 -- | compact Compact a list of values with the following rules:
 -- - Nothing are removed
 -- - If two remaining onsecutive values are equal,
 --     they are replaced by one value equal to the successor of those values
--- - the previous rule is not recursive
+-- - previous replacement are not recursive
 -- - Other values are kept
 --
 -- Examples
@@ -54,22 +99,15 @@ pushTo = undefined
 -- >>> compact $ map Just "hello"
 -- "hemo"
 --
--- prop> [succ x] == (compact . replicate 2 . Just) (x :: Int)
--- prop> [succ x, x] == (compact . replicate 3 . Just) (x :: Int)
--- prop> replicate 2 (succ x) == (compact . replicate 4 . Just) (x :: Int)
---
 compact :: (Enum a, Eq a) => [Maybe a] -> [a]
-compact = 
-  map (flip maybe succ <$> head <*> listToMaybe . tail)
-    . ((takeWhile (not . null) . map (take 2) . iterate (drop 2)) =<<)
-    . group . catMaybes
-
+compact = let
+  compact' (x:y:xs) | x == y    = succ x : compact' xs
+                    | otherwise = x : compact' (y:xs)
+  compact' xs                   = xs
+  in compact' . catMaybes
 
 -- Arbitrary instances
 
--- Build an Arbitrary instance for Board of any given size
---
--- The external list length is equal to the length of each internal lists
 instance Arbitrary a => Arbitrary (Board a) where 
   arbitrary = liftM Board (arbitrary >>= replicateM <*> vector)
 
