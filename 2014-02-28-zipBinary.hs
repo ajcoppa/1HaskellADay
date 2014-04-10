9fa8c6718a58f61e46af647ce3386eaaf0cb07d6
AJ Coppa <aj.coppa@gmail.com>
2014-04-09 22:17:50 -0400

    Added zipBinary

---
+++ 2014-02-28-zipBinary.hs
@@ -0,0 +1,12 @@
+import Control.Applicative
+
+-- use elements of the first lists to zip elements
+-- of the second and third lists
+-- zipBinary [(+), (*), (*)] [2,2,2] [4,4,4] == [6,8,8]
+-- zipBinary [(+)] [2,2,2] [4,4,4] == [6]
+
+zipBinary :: [a->b->c] -> [a] -> [b] -> [c]
+zipBinary ops xs ys = undefined
+
+main = print $
+  zipBinary (cycle [(+), (*)]) [1 .. 4] [2..5] -- [3, 6, 7, 20]
