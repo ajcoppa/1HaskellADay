f0a926362c812b110e5db8c788b752e83634d7f8
AJ Coppa <aj.coppa@gmail.com>
2014-04-10 19:40:27 -0400

    Finished (spoiled) onBothSide

---
+++ 2014-04-10-onBothSide.hs
@@ -0,0 +1,24 @@
+module HAD.Y2014.M04.D10.Exercise where
+
+import Control.Monad (join)
+
+{- | onBothSide
+   Apply the same argument on both side of a binary numeric function.
+
+   The only interesting solution is point-free.
+
+   It IS the easiest exercise so far, with the shortest solution.
+
+   Examples:
+   >>> onBothSide (+) 4
+   8
+
+   >>> onBothSide (*) 4
+   16
+-}
+
+-- They spoiled this by tweeting out the solution. :(
+onBothSide :: (a -> a -> b) -> a -> b
+onBothSide f x = join f x
+
+main = print ""
