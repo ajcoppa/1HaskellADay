18f4fb08a18b14bd9ed122a0e4b870522bc7b57c
AJ Coppa <aj.coppa@gmail.com>
2014-04-16 19:04:13 -0400

    Finished reverseMap

--- 2014-04-16-reverseMap.hs
+++ 2014-04-16-reverseMap.hs
@@ -1,7 +1,17 @@
 module HAD.Y2014.M04.D16.Exercise where
 
+import Data.Char (toUpper)
+
 {- | reverseMap
    No definition, look to types
 -}
 reverseMap :: [a -> b] -> a -> [b]
-reverseMap = undefined
+reverseMap [] x = []
+reverseMap (f:fs) x = f x : reverseMap fs x
+
+reverseMap' :: [a -> b] -> a -> [b]
+reverseMap' fs x = map ($ x) fs
+
+main = do
+  print $ reverseMap' [id, (+1), (+2)] 0
+  print $ reverseMap' [(++ "!"), ("Hmm. " ++), map toUpper] "I'm a teapot"
