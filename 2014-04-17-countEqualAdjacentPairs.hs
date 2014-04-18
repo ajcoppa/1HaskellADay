e8460c9ec5b516a2a10376b6e915ac9ccc02ef1d
AJ Coppa <aj.coppa@gmail.com>
2014-04-17 23:29:51 -0400

    Added countEqualAdjacentPairs

---
+++ 2014-04-17-countEqualAdjacentPairs.hs
@@ -0,0 +1,20 @@
+{- | countEqualAdjacentPairs
+
+   Count adjacent pairs (vertical or horizpontal) of values that are equal in
+   a matrix.
+
+   Examples:
+
+   >>> countEqualAdjacentPairs [[0,1],[0,2]]
+   1
+   >>> countEqualAdjacentPairs [[0,0],[1,2]]
+   1
+   >>> countEqualAdjacentPairs [[0,1],[0,0]]
+   2
+   >>> countEqualAdjacentPairs [['a','a'],['a','a']]
+   4
+   >>> countEqualAdjacentPairs [[0,0,0],[0,0,0],[0,0,0]]
+   12
+-}
+countEqualAdjacentPairs :: Eq a => [[a]] -> Int
+countEqualAdjacentPairs = undefined
